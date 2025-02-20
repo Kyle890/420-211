5088294162d1aa1332745289fd112875a3f8d17e
Nyle Kersen Joseph <6290712@cegepmv.ca>
2025-02-20 17:36:14 -0500

    react test

---
+++ Exo-diriges/REACT_MAISON/Exo1_test/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import MainSection from "./MainSection";
+import './App.css';
+
+function App() {
+    return(
+      <>
+        <Header />
+        <MainSection />
+        <Footer />
+      </>
+    );
+}
+
+export default App
+
