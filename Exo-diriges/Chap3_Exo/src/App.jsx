d6ca73d4bf08db374c93961a66ae1d9841dfa498
Nyle Kersen Joseph <6290712@cegepmv.ca>
2025-03-20 17:55:51 -0400

    Exo 20 mars

--- Exo-diriges/Chap3_Exo/src/App.jsx
+++ Exo-diriges/Chap3_Exo/src/App.jsx
@@ -44,7 +44,7 @@ import { useState } from "react"
   );
 }*/
 
-export default function App() {
+/*export default function App() {
 
   const [myFavoriteThings, setMyFavoriteThings] = useState([]);
   
@@ -68,4 +68,54 @@ export default function App() {
       </section>
     </main>
   )
+}*/
+
+
+import avatar from "./images/user.png"
+import starFilled from "./images/star-filled.png"
+import starEmpty from "./images/star-empty.png"
+
+export default function App() {
+    const [contact, setContact] = useState({
+        firstName: "John",
+        lastName: "Doe",
+        phone: "+1 (514) 555-1212",
+        email: "itsmyrealname@example.com",
+        isFavorite: false
+    })
+
+    function toggleFavorite() {
+        console.log("Toggle Favorite")
+    }
+
+    return (
+        <main>
+            <article className="card">
+                <img
+                    src={avatar}
+                    className="avatar"
+                    alt="Photo de profil de John Doe"
+                />
+                <div className="info">
+                    <button
+                        onClick={toggleFavorite}
+                        aria-pressed={false}
+                        className="favorite-button"
+                    >
+                        <img
+                            src={starEmpty}
+                            alt="icône étoile vide"
+                            className="favorite"
+                        />
+                    </button>
+                    <h2 className="name">
+                        John Doe
+                    </h2>
+                    <p className="contact">+1 (212) 555-1212</p>
+                    <p className="contact">itsmyrealname@example.com</p>
+                </div>
+
+            </article>
+        </main>
+    )
 }
